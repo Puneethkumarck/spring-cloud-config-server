d0d5bba3334d1bde7cab30460de019377480386d
PuneethKumarCk <punith.564@gmail.com>
2017-11-27 19:09:58 +0530

    initial commit

---
+++ src/main/java/com/home/work/springcloudconfigtest/SpringCloudConfigTestApplication.java
@@ -0,0 +1,13 @@
+package com.home.work.springcloudconfigtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringCloudConfigTestApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigTestApplication.class, args);
+	}
+}
